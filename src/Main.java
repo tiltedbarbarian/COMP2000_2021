238b59a69021b1bf06973870b10bdb7f41cd9963
Damian Jurd <damian.jurd@mq.edu.au>
2021-08-05 16:55:34 +1000

    tasks 3, 4, and 5 solutions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+class Main extends JFrame {
+    
+    class App extends JPanel {
+        
+        Grid grid;
+
+        public App() {
+            setPreferredSize(new Dimension(720, 720));
+            grid = new Grid();
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            grid.paint(g, getMousePosition());
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
+        window.run();
+    }
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        App canvas = new App();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public void run() {
+        while (true) {
+            this.repaint();
+        }
     }
 }
